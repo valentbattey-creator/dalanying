702287b9fa6ea57dfc2ceeb15f9b2b6ac85e275c
valentbattey-creator <valentbattey@github.com>
2026-08-11 20:54:43 +0800

    fix: grid items-start to prevent equal height whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -270,7 +270,7 @@ export default function HomePage() {
                 ))}
 
                 {/* Post grid - responsive grid */}
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-5" style={{ paddingTop: 8 }}>
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-5 items-start" style={{ paddingTop: 8 }}>
                   {sorted.map((p, i) => (
                     <React.Fragment key={p.id}>
                       <div>
